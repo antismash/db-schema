5bcbb3e846c370f3feb2243143aa98ff27ee4ed0
Kai Blin <kblin@biosustain.dtu.dk>
2016-06-21 16:39:49 +0200

    Add n:m mapping for clusters to bgc_types
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

---
+++ rel_clusters_types.sql
@@ -0,0 +1,8 @@
+--- Many to many relation for biosynthetic_gene_clusters and bgc_types
+CREATE TABLE rel_clusters_types (
+    bgc_id	int4,
+    bgc_type_id	int4,
+    CONSTRAINT rel_clusters_types_pkey PRIMARY KEY (bgc_id, bgc_type_id),
+    CONSTRAINT rel_clusters_types_bgc_id_fkey FOREIGN KEY (bgc_id) REFERENCES antismash.biosynthetic_gene_clusters (bgc_id),
+    CONSTRAINT rel_clusters_types_bgc_type_id_fkey FOREIGN KEY (bgc_type_id) REFERENCES antismash.bgc_types (bgc_type_id)
+);
