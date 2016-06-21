b3c3a8910f60d996b484cf38b2f5a941a3a1031f
Kai Blin <kblin@biosustain.dtu.dk>
2016-06-21 14:14:05 +0200

    init_database: More complete bootstrap for the database
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- init_database.sh
+++ init_database.sh
@@ -7,16 +7,21 @@ PSQL_HOST=localhost
 PSQL_PORT=15432
 PSQL_USER=postgres
 PSQL_DB=antismash
+PSQL_SCHEMA=antismash
 
-PSQL="psql -h $PSQL_HOST -p $PSQL_PORT -U $PSQL_USER $PSQL_DB"
+PSQL="psql -h $PSQL_HOST -p $PSQL_PORT -U $PSQL_USER"
+PSQL_AS="$PSQL $PSQL_DB"
+
+$PSQL -tc "SELECT 1 FROM pg_database WHERE datname = '${PSQL_DB}';" | grep -q 1 || $PSQL -c "CREATE DATABASE $PSQL_DB;"
+$PSQL_AS -c "CREATE SCHEMA IF NOT EXISTS ${PSQL_SCHEMA};"
 
 for t in sampling_sites evidences samples isolates genomes dna_sequences loci operons gene_functional_classes genes; do
     echo "Processing $t"
     if [ -n "$ASDB_REINIT" ]; then
         echo "Dropping old table content"
-        echo "DROP TABLE IF EXISTS ${PSQL_DB}.${t} CASCADE;" | $PSQL
+        $PSQL_AS -c "DROP TABLE IF EXISTS ${PSQL_SCHEMA}.${t} CASCADE;"
     else
         echo "Just initialising new content"
     fi
-    $PSQL < "${t}.sql"
+    $PSQL_AS < "${t}.sql"
 done
