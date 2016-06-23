714499e9a2fb133c490223059f298a48b3d7e45a
Kai Blin <kblin@biosustain.dtu.dk>
2016-06-23 12:46:56 +0200

    Make proper use of the schema for reinits
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- init_database.sh
+++ init_database.sh
@@ -13,6 +13,12 @@ PSQL="psql -h $PSQL_HOST -p $PSQL_PORT -U $PSQL_USER"
 PSQL_AS="$PSQL $PSQL_DB"
 
 $PSQL -tc "SELECT 1 FROM pg_database WHERE datname = '${PSQL_DB}';" | grep -q 1 || $PSQL -c "CREATE DATABASE $PSQL_DB;"
+
+if [ -n "$ASDB_REINIT" ]; then
+    echo "Clearing out the whole schema"
+    $PSQL_AS -c "DROP SCHEMA IF EXISTS ${PSQL_SCHEMA} CASCADE;"
+fi
+
 $PSQL_AS -c "CREATE SCHEMA IF NOT EXISTS ${PSQL_SCHEMA};"
 
 # tables not depending on other tables
@@ -26,11 +32,5 @@ TABLES="$TABLES rel_clusters_types rel_clusters_compounds rel_compounds_monomers
 
 for t in $TABLES; do
     echo "Processing $t"
-    if [ -n "$ASDB_REINIT" ]; then
-        echo "Dropping old table content"
-        $PSQL_AS -c "DROP TABLE IF EXISTS ${PSQL_SCHEMA}.${t} CASCADE;"
-    else
-        echo "Just initialising new content"
-    fi
     $PSQL_AS < "${t}.sql"
 done
