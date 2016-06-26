5f29e0eb7961457d2a5dc647c8c0340546d27f70
Kai Blin <kblin@biosustain.dtu.dk>
2016-06-26 16:51:55 +0200

    bgc_types: add missing types
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- bgc_types.sql
+++ bgc_types.sql
@@ -17,7 +17,7 @@ SELECT val.term, val.description, val.parent::int4
 FROM (
     VALUES
         ('pks', 'Polyketide', NULL),
-        ('nrp', 'Nonribosomal peptide', NULL),
+        ('nrps', 'Nonribosomal peptide', NULL),
         ('ripp', 'Ribosomally synthesized and post-translationally modified peptide', NULL),
         ('terpene', 'Terpene', NULL),
         ('saccharide', 'Saccharide', NULL),
@@ -33,7 +33,41 @@ FROM (
     VALUES
         ('t1pks', 'Type I polyketide', 'pks'),
         ('transatpks', 'Trans-AT polyketide', 'pks'),
-        ('lantipeptide', 'Lanthipeptide', 'ripp')
-        --- TODO: Add all missing types
+        ('t2pks', 'Type II polyketide', 'pks'),
+        ('t3pks', 'Type III polyketide', 'pks'),
+        ('otherks', 'Other types of polyketides', 'pks'),
+        ('arylpolyene', 'Aryl polyene', 'other'),
+        ('resorcinol', 'Resorcinol', 'other'),
+        ('ladderane', 'Ladderane', 'other'),
+        ('pufa', 'Polyunsaturated fatty acid', 'other'),
+        ('lantipeptide', 'Lanthipeptide', 'ripp'),
+        ('bacteriocin', 'Bacteriocin or other unspecified RiPP', 'ripp'),
+        ('thiopeptide', 'Thipeptide', 'ripp'),
+        ('linaridin', 'Linaridin', 'other'),
+        ('cyanobactin', 'Cyanobactin', 'ripp'),
+        ('glycocin', 'Glycocin', 'other'),
+        ('lap', 'Goadsporin-like', 'other'),
+        ('lassopeptide', 'Lasso peptode', 'ripp'),
+        ('sactipeptide', 'Sactipeptide', 'ripp'),
+        ('bottromycin', 'Bottromycin-like', 'ripp'),
+        ('head_to_tail', 'Head-to-tail cyclised (subtilosin-like)', 'ripp'),
+        ('microcin', 'Microcin', 'ripp'),
+        ('microviridin', 'Microviridin', 'ripp'),
+        ('proteusin', 'Proteusin', 'ripp'),
+        ('blactam', 'beta-lactam', 'other'),
+        ('amglyccycl', 'Aminoglycoside/aminocyclitol', 'other'),
+        ('aminocoumarin', 'Aminocoumarin', 'other'),
+        ('siderophore', 'Siderophore', 'other'),
+        ('ectoine', 'Ectoine', 'other'),
+        ('butyrolactone', 'Butyrolactone', 'other'),
+        ('indole', 'Indole', 'other'),
+        ('nucleoside', 'Nucleoside', 'other'),
+        ('phosphoglycolipid', 'Phosphoglycolipid', 'other'),
+        ('melanin', 'Melanin', 'other'),
+        ('oligosaccharide', 'Oligosaccharide', 'saccharide'),
+        ('furan', 'Furan', 'other'),
+        ('hserlactone', 'Homoserine lactone', 'other'),
+        ('phenazine', 'Phenazine', 'other'),
+        ('phosphonate', 'Phosphonate', 'other')
     ) val ( term, description, parent_term )
 LEFT JOIN antismash.bgc_types f ON val.parent_term = f.term;
