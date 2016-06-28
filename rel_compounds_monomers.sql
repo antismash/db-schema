6b9b001883841a977544311da961fb9d137841f3
Kai Blin <kblin@biosustain.dtu.dk>
2016-06-28 08:07:27 +0200

    rel_compounds_monomers: Only the set of compound, monomer and position needs to be unique
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- rel_compounds_monomers.sql
+++ rel_compounds_monomers.sql
@@ -2,8 +2,8 @@
 CREATE TABLE antismash.rel_compounds_monomers (
     compound_id	int4,
     monomer_id	int4,
-    position_in_compound	int4,
-    CONSTRAINT rel_compounds_monomers_pkey PRIMARY KEY (compound_id, monomer_id),
+    position	int4,
+    CONSTRAINT rel_compounds_monomers_pkey PRIMARY KEY (compound_id, monomer_id, position),
     CONSTRAINT rel_compounds_monomers_compound_id_fkey FOREIGN KEY (compound_id) REFERENCES antismash.compounds (compound_id),
     CONSTRAINT rel_compounds_monomers_monomer_id_fkey FOREIGN KEY (monomer_id) REFERENCES antismash.monomers (monomer_id)
 );
