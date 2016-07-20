0a16157854b62dc39e1957c42d2887a9cb14b555
Kai Blin <kblin@biosustain.dtu.dk>
2016-07-20 11:24:00 +0200

    genes: Drop foreign key relation to smcog_hits
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- genes.sql
+++ genes.sql
@@ -11,15 +11,14 @@ CREATE TABLE antismash.genes (
     translation	text,
     locus	int4,
     operon	int4,
-    smcog_hit	int4,
     CONSTRAINT genes_pkey PRIMARY KEY (gene_id),
     CONSTRAINT genes_functional_class_fkey FOREIGN KEY (functional_class) REFERENCES antismash.functional_classes (functional_class_id),
     CONSTRAINT genes_evidence_fkey FOREIGN KEY (evidence) REFERENCES antismash.evidences (evidence_id),
     CONSTRAINT genes_locus_fkey FOREIGN KEY (locus) REFERENCES antismash.loci (locus_id) ON DELETE CASCADE,
-    CONSTRAINT genes_operon_fkey FOREIGN KEY (operon) REFERENCES antismash.operons (operon_id),
-    CONSTRAINT genes_smcog_hit_fkey FOREIGN KEY (smcog_hit) REFERENCES antismash.smcog_hits (smcog_hit_id)
+    CONSTRAINT genes_operon_fkey FOREIGN KEY (operon) REFERENCES antismash.operons (operon_id)
 );
 COMMENT ON TABLE antismash.genes IS
   'A gene record.';
 
 CREATE INDEX genes_locus_idx ON antismash.genes (locus);
+CREATE INDEX genes_locus_tag_idx ON antismash.genes (locus_tag);
