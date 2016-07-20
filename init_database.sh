d36dfe348d74c6bb0f76130c52655a67b539dabc
Kai Blin <kblin@biosustain.dtu.dk>
2016-07-20 11:26:19 +0200

    smcog_hits: Add foreign key referencing the bgc
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- init_database.sh
+++ init_database.sh
@@ -25,11 +25,11 @@ $PSQL_AS -c "CREATE SCHEMA IF NOT EXISTS ${PSQL_SCHEMA};"
 TABLES="sampling_sites evidences bgc_types compounds monomers taxa"
 # tables depending on other tables. Please keep the order intact
 TABLES="$TABLES bgc_rules samples isolates genomes dna_sequences loci operons "
-TABLES="$TABLES functional_classes smcogs smcog_hits genes biosynthetic_gene_clusters"
+TABLES="$TABLES functional_classes smcogs genes biosynthetic_gene_clusters"
 TABLES="$TABLES as_domains clusterblast_algorithms clusterblast_hits"
 # tables that create many-to-many relations
 TABLES="$TABLES rel_clusters_types rel_clusters_compounds rel_compounds_monomers"
-TABLES="$TABLES rel_as_domains_monomers"
+TABLES="$TABLES rel_as_domains_monomers smcog_hits"
 
 
 for t in $TABLES; do
--- smcog_hits.sql
+++ smcog_hits.sql
@@ -1,11 +1,12 @@
 CREATE TABLE antismash.smcog_hits (
-    smcog_hit_id	serial NOT NULL,
+    smcog_id	int4,
+    gene_id	int4,
     score	float8,
     evalue	float8,
     image_path	text,
-    smcog_class	int4,
-    CONSTRAINT smcog_hits_pkey PRIMARY KEY (smcog_hit_id),
-    CONSTRAINT smcog_hits_smcog_class_fkey FOREIGN KEY (smcog_class) REFERENCES antismash.smcogs (smcog_id)
+    CONSTRAINT smcog_hits_pkey PRIMARY KEY (smcog_id, gene_id),
+    CONSTRAINT smcog_hits_gene_id_fkey FOREIGN KEY (gene_id) REFERENCES antismash.genes (gene_id) ON DELETE CASCADE,
+    CONSTRAINT smcog_hits_smcog_id_fkey FOREIGN KEY (smcog_id) REFERENCES antismash.smcogs (smcog_id)
 );
 COMMENT ON TABLE antismash.smcog_hits IS
   'Metadata about an smCoG hit';
