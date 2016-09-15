1609b32df01811a8e766e8ddeaea0589e575b5cf
Kai Blin <kblin@biosustain.dtu.dk>
2016-09-15 17:13:09 +0200

    isolates: Rename the foreign key to follow SQL conventions
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- isolates.sql
+++ isolates.sql
@@ -1,9 +1,9 @@
 CREATE TABLE antismash.isolates
 (
   isolate_id	serial NOT NULL,
-  sample	int4,
+  sample_id	int4,
   CONSTRAINT isolates_pkey PRIMARY KEY (isolate_id),
-  CONSTRAINT isolates_sample_fkey FOREIGN KEY (sample) REFERENCES antismash.samples (sample_id)
+  CONSTRAINT isolates_sample_id_fkey FOREIGN KEY (sample_id) REFERENCES antismash.samples (sample_id)
 );
 COMMENT ON TABLE antismash.isolates
   IS 'A single biological isolate, sample or strain';
