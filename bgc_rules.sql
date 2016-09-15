2eff7938a572a18b3ef996e8dfe308349819ec36
Kai Blin <kblin@biosustain.dtu.dk>
2016-09-15 17:09:25 +0200

    bgc_rules: Rename the foreign key to follow SQL conventions
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- bgc_rules.sql
+++ bgc_rules.sql
@@ -1,9 +1,9 @@
 CREATE TABLE antismash.bgc_rules (
     bgc_rule_id	serial NOT NULL,
     rule	text,
-    bgc_type	int4,
+    bgc_type_id	int4,
     CONSTRAINT bgc_rules_pkey PRIMARY KEY (bgc_rule_id),
-    CONSTRAINT bgc_rules_bgc_type_fkey FOREIGN KEY (bgc_type) REFERENCES antismash.bgc_types (bgc_type_id)
+    CONSTRAINT bgc_rules_bgc_type_id_fkey FOREIGN KEY (bgc_type_id) REFERENCES antismash.bgc_types (bgc_type_id)
 );
 
 COMMENT ON TABLE antismash.bgc_rules IS
