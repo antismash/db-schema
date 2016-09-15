7839ed2c092b2b5893e0aa9d547ef09affc2e133
Kai Blin <kblin@biosustain.dtu.dk>
2016-09-15 17:12:53 +0200

    samples: Rename the foreign key to follow SQL conventions
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- samples.sql
+++ samples.sql
@@ -4,9 +4,9 @@ CREATE TABLE antismash.samples (
     sample_id	serial NOT NULL,
     habitat	text,
     time	text,
-    site	int4,
+    site_id	int4,
     CONSTRAINT samples_pkey PRIMARY KEY (sample_id),
-    CONSTRAINT samples_site_fkey FOREIGN KEY (site) REFERENCES antismash.sampling_sites (site_id)
+    CONSTRAINT samples_site_id_fkey FOREIGN KEY (site_id) REFERENCES antismash.sampling_sites (site_id)
 );
 COMMENT ON TABLE antismash.samples IS
   'A biological sample.';
