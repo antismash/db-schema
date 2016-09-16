52c7aa8c29b55ba9069731d982529544d3edc90b
Kai Blin <kblin@biosustain.dtu.dk>
2016-09-16 11:12:04 +0200

    functional_classes: Rename foreign keys to follow SQL conventions
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- functional_classes.sql
+++ functional_classes.sql
@@ -1,27 +1,27 @@
 CREATE TABLE antismash.functional_classes (
     functional_class_id	serial NOT NULL,
     name	text,
-    parent	int4,
+    parent_id	int4,
     CONSTRAINT functional_classes_pkey PRIMARY KEY (functional_class_id),
-    CONSTRAINT functional_classes_parent_fkey FOREIGN KEY (parent) REFERENCES antismash.functional_classes (functional_class_id)
+    CONSTRAINT functional_classes_parent_id_fkey FOREIGN KEY (parent_id) REFERENCES antismash.functional_classes (functional_class_id)
 );
 COMMENT ON TABLE antismash.functional_classes IS
   'An index of possible functional classes';
 
 --- Basic functional classes
-INSERT INTO antismash.functional_classes (name, parent)
-SELECT val.name, val.parent::int4
+INSERT INTO antismash.functional_classes (name, parent_id)
+SELECT val.name, val.parent_id::int4
 FROM (
     VALUES
         ('biosynthetic', NULL),
         ('regulator', NULL),
         ('transporter', NULL),
         ('other', NULL)
-    ) val ( name, parent );
+    ) val ( name, parent_id );
 
 
 --- Advanced classes
-INSERT INTO antismash.functional_classes (name, parent)
+INSERT INTO antismash.functional_classes (name, parent_id)
 SELECT val.name, f.functional_class_id
 FROM (
     VALUES
