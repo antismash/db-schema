160164b1b1ae68a6c98be2b9b23d15ced5a4d83d
Kai Blin <kblin@biosustain.dtu.dk>
2016-09-16 09:53:08 +0200

    dna_sequences: rename the foreign key to follow SQL conventions
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- dna_sequences.sql
+++ dna_sequences.sql
@@ -6,8 +6,8 @@ CREATE TABLE antismash.dna_sequences (
     contig_type	int4,
     chromosome_type	int4,
     version	int4,
-    genome	int4,
+    genome_id	int4,
     CONSTRAINT dna_sequences_pkey PRIMARY KEY (sequence_id),
-    CONSTRAINT dna_sequences_genome_fkey FOREIGN KEY (genome) REFERENCES antismash.genomes (genome_id) ON DELETE CASCADE
+    CONSTRAINT dna_sequences_genome_id_fkey FOREIGN KEY (genome_id) REFERENCES antismash.genomes (genome_id) ON DELETE CASCADE
 );
 
--- view_gene_cluster_map.sql
+++ view_gene_cluster_map.sql
@@ -1,10 +1,10 @@
 CREATE MATERIALIZED VIEW antismash.gene_cluster_map AS
 SELECT sequence_id, bgc_id, gene_id FROM (
     SELECT
-        gene_id, start_pos, end_pos, sequence_id
+        gene_id, start_pos, end_pos, seq.sequence_id
     FROM antismash.genes g
     JOIN antismash.loci l ON g.locus = l.locus_id
-    JOIN antismash.dna_sequences seq ON l.sequence = seq.sequence_id
+    JOIN antismash.dna_sequences seq USING (sequence_id)
     ORDER BY
     gene_id
 ) e1
@@ -13,7 +13,7 @@ LEFT JOIN LATERAL (
         bgc_id
     FROM antismash.biosynthetic_gene_clusters bgc
     JOIN antismash.loci l ON bgc.locus = l.locus_id
-    JOIN antismash.dna_sequences seq ON l.sequence = seq.sequence_id
+    JOIN antismash.dna_sequences seq USING (sequence_id)
     WHERE
         seq.sequence_id = e1.sequence_id AND
         int4range(l.start_pos, l.end_pos) @> int4range(e1.start_pos, e1.end_pos)
