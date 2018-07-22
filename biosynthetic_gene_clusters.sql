9d2852c0e954399f736844adeb2181720d9dd83b
Kai Blin <kblin@biosustain.dtu.dk>
2018-07-22 13:04:26 +0200

    bgc: Keep track of minimal and contig_edge status per cluster
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- biosynthetic_gene_clusters.sql
+++ biosynthetic_gene_clusters.sql
@@ -3,6 +3,8 @@ CREATE TABLE antismash.biosynthetic_gene_clusters (
     cluster_number	int4,
     locus_id	int4,
     evidence_id	int4,
+    contig_edge	bool,
+    minimal	bool,
     CONSTRAINT biosynthetic_gene_clusters_pkey PRIMARY KEY (bgc_id),
     CONSTRAINT biosynthetic_gene_clusters_locus_id_fkey FOREIGN KEY (locus_id) REFERENCES antismash.loci (locus_id) ON DELETE CASCADE,
     CONSTRAINT biosynthetic_gene_clusters_evidence_id_fkey FOREIGN KEY (evidence_id) REFERENCES antismash.evidences (evidence_id)
