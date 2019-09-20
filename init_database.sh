9ab5bdf1abed9fa4c688f9b3af7dda8a7c51d006
Simon Shaw <SJShaw@users.noreply.github.com>
2019-09-20 15:23:26 +0200

    pfams: update to pfam 31.0

--- init_database.sh
+++ init_database.sh
@@ -22,12 +22,12 @@ $PSQL_AS -c "CREATE SCHEMA IF NOT EXISTS ${PSQL_SCHEMA};"
 
 # tables not depending on other tables
 TABLES="sampling_sites bgc_types compounds monomers taxa profiles "
-TABLES="$TABLES as_domain_profiles "
+TABLES="$TABLES as_domain_profiles pfams "
 # tables depending on other tables. Please keep the order intact
 TABLES="$TABLES bgc_rules samples isolates genomes dna_sequences loci "
 TABLES="$TABLES functional_classes smcogs cdss genes biosynthetic_gene_clusters"
 TABLES="$TABLES as_domains clusterblast_algorithms clusterblast_hits tta_codons"
-TABLES="$TABLES pfams pfam_domains filenames"
+TABLES="$TABLES pfam_domains filenames"
 # tables that create many-to-many relations
 TABLES="$TABLES rel_clusters_types rel_clusters_compounds rel_compounds_monomers"
 TABLES="$TABLES rel_as_domains_monomers smcog_hits profile_hits "
