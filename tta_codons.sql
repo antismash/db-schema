3f7a2c40fb0d4c77cc4ee3e44fa57018fd1ee6c9
Simon Shaw <SJShaw@users.noreply.github.com>
2019-11-01 17:29:50 +0100

    tta: add link to record

--- tta_codons.sql
+++ tta_codons.sql
@@ -1,5 +1,6 @@
 CREATE TABLE antismash.tta_codons (
     tta_codon_id	serial NOT NULL,
+    seq_id  text    NOT NULL REFERENCES antismash.dna_sequences (accession) ON DELETE CASCADE,
     location text NOT NULL,
     CONSTRAINT tta_codon_pkey PRIMARY KEY (tta_codon_id)
 );
