acc58be5dae1c72f385774c54bc2847e7c3eb660
Simon Shaw <SJShaw@users.noreply.github.com>
2019-11-01 17:55:28 +0100

    as_domains: link as_domains to modules

--- as_domains.sql
+++ as_domains.sql
@@ -14,6 +14,8 @@ CREATE TABLE antismash.as_domains (
     as_domain_profile_id	int4,
     location text NOT NULL,
     cds_id	int4,
+    module_id int4 NOT NULL REFERENCES antismash.modules ON DELETE CASCADE,
+    function_id int4 NOT NULL REFERENCES antismash.module_domain_functions ON DELETE CASCADE,
     CONSTRAINT as_domains_pkey PRIMARY KEY (as_domain_id),
     CONSTRAINT as_domains_as_domain_profile_id_fkey FOREIGN KEY (as_domain_profile_id) REFERENCES antismash.as_domain_profiles (as_domain_profile_id) ON DELETE CASCADE,
     CONSTRAINT as_domains_cds_id_fkey FOREIGN KEY (cds_id) REFERENCES antismash.cdss (cds_id) ON DELETE CASCADE
