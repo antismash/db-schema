fde3979863afba6a33aaae637cfe1ae558fb6705
Simon Shaw <SJShaw@users.noreply.github.com>
2023-05-10 13:23:04 +0200

    as_domains: allow linking arbitrary domain subtypes to domains

--- as_domains.sql
+++ as_domains.sql
@@ -26,3 +26,22 @@ COMMENT ON TABLE antismash.as_domains IS
   'A functional domain on a gene as predicted by antiSMASH';
 
 CREATE INDEX as_domains_cds_id_idx ON antismash.as_domains (cds_id);
+
+
+CREATE TABLE antismash.as_domain_subtypes (
+    subtype	text	PRIMARY KEY,
+    description	text
+);
+
+COMMENT ON TABLE antismash.as_domain_subtypes IS
+  'Possible subtypes of domains predicted by antiSMASH, e.g. TransAT-KS';
+
+
+CREATE TABLE antismash.rel_as_domain_to_subtype (
+    as_domain_id	int4	NOT NULL REFERENCES antismash.as_domains,
+    subtype	text	NOT NULL REFERENCES antismash.as_domain_subtypes,
+    CONSTRAINT rel_as_domain_to_subtype_pkey PRIMARY KEY (as_domain_id, subtype)
+);
+
+COMMENT ON TABLE antismash.as_domain_subtypes IS
+  'Relationships between a specific aSDomain and subtypes';
