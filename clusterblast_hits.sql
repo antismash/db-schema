19afaa003497398451949a9afe6e019ca401d836
Simon Shaw <SJShaw@users.noreply.github.com>
2023-06-23 13:20:06 +0200

    clusterblast_hits: remove unused fields

--- clusterblast_hits.sql
+++ clusterblast_hits.sql
@@ -1,18 +1,11 @@
 CREATE TABLE antismash.clusterblast_hits (
-    clusterblast_hit_id	serial NOT NULL,
-    region_id int4 REFERENCES antismash.regions ON DELETE CASCADE,
-    candidate_id int4 REFERENCES antismash.candidates ON DELETE CASCADE,
-    protocluster_id int4 REFERENCES antismash.protoclusters ON DELETE CASCADE,
-    rank	int4,
-    acc	text,
-    description	text,
-    similarity	int4,
-    algorithm_id	int4,
-    CONSTRAINT clusterblast_hits_pkey PRIMARY KEY (clusterblast_hit_id),
-    CONSTRAINT clusterblast_hits_algorithm_id_fkey FOREIGN KEY (algorithm_id) REFERENCES antismash.clusterblast_algorithms (algorithm_id),
-    CONSTRAINT clusterblast_hits_require_single_reference CHECK (
-        (region_id IS NOT NULL)::integer + (candidate_id IS NOT NULL)::integer + (protocluster_id IS NOT NULL)::integer = 1
-    )
+    clusterblast_hit_id	serial PRIMARY KEY,
+    region_id int4 REFERENCES antismash.regions ON DELETE CASCADE NOT NULL,
+    rank	int4 NOT NULL,
+    acc	text NOT NULL,
+    description	text NOT NULL,
+    similarity	int4 NOT NULL,
+    algorithm_id	int4 REFERENCES antismash.clusterblast_algorithms
 );
 
 COMMENT ON TABLE antismash.clusterblast_hits IS
