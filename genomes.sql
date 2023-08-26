9774005821b8228d8a7855f81c63a47850d17211
Kai Blin <kblin@biosustain.dtu.dk>
2023-08-26 20:23:45 +0200

    genomes: tax_id and assembly_id cannot be NULL, enforce it in the schema
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- genomes.sql
+++ genomes.sql
@@ -1,10 +1,10 @@
 CREATE TABLE antismash.genomes (
     genome_id	serial NOT NULL,
-    tax_id	int4,
+    tax_id	int4 NOT NULL,
     bio_project	text,
     bio_sample	text,
     isolate_id	int4,
-    assembly_id	text,
+    assembly_id	text NOT NULL,
     CONSTRAINT genomes_pkey PRIMARY KEY (genome_id),
     CONSTRAINT genomes_tax_id_fkey FOREIGN KEY (tax_id) REFERENCES antismash.taxa (tax_id) ON DELETE CASCADE,
     CONSTRAINT genomes_isolate_id_fkey FOREIGN KEY (isolate_id) REFERENCES antismash.isolates (isolate_id) ON DELETE CASCADE
