8d02398614f1d40a093832c408c81c53f9929999
Kai Blin <kblin@biosustain.dtu.dk>
2023-08-28 16:17:03 +0200

    regions: None of the antiSMASH-predicted fields should ever be null
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- regions.sql
+++ regions.sql
@@ -1,11 +1,11 @@
 CREATE TABLE antismash.regions (
     region_id	serial NOT NULL,
     accession text NOT NULL REFERENCES antismash.dna_sequences (accession) ON DELETE CASCADE,
-    region_number	int4,
-    location	text,
-    start_pos	int4,
-    end_pos	int4,
-    contig_edge	bool,
+    region_number	int4 NOT NULL,
+    location	text NOT NULL,
+    start_pos	int4 NOT NULL,
+    end_pos	int4 NOT NULL,
+    contig_edge	bool NOT NULL,
     CONSTRAINT regions_pkey PRIMARY KEY (region_id)
 );
 
