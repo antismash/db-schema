5842dee27a9443bdf61df6ca83ffc7b62ae79968
Kai Blin <kblin@biosustain.dtu.dk>
2023-08-31 06:24:52 +0200

    dna_sequences: move the primary key contraint closer to where the key is defined
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- dna_sequences.sql
+++ dna_sequences.sql
@@ -1,14 +1,13 @@
 CREATE TABLE antismash.dna_sequences (
     dna	text,
     md5	text,
-    accession	text NOT NULL,
+    accession	text PRIMARY KEY,
     definition	text,
     contig_type	int4,
     chromosome_type	int4,
     record_number	int4 NOT NULL,
     version	int4,
     genome_id	int4,
-    CONSTRAINT dna_sequences_pkey PRIMARY KEY (accession),
     CONSTRAINT dna_sequences_genome_id_fkey FOREIGN KEY (genome_id) REFERENCES antismash.genomes (genome_id) ON DELETE CASCADE
 );
 
