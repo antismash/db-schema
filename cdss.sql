2860ff0d9a23e786f6a938390d0fed1b4a68cc10
Kai Blin <kblin@biosustain.dtu.dk>
2025-06-30 22:26:41 +0200

    cdss: Add a new gene/function mapping table
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- cdss.sql
+++ cdss.sql
@@ -1,4 +1,4 @@
-CREATE TABLE antismash.cdss (
+CREATE TABLE IF NOT EXISTS antismash.cdss (
     cds_id	serial NOT NULL,
     functional_class_id	int4,
     locus_tag	text,
@@ -14,4 +14,17 @@ CREATE TABLE antismash.cdss (
 COMMENT ON TABLE antismash.cdss IS
   'A CDS record.';
 
-CREATE INDEX cdss_locus_tag_idx ON antismash.cdss (locus_tag);
+CREATE INDEX IF NOT EXISTS cdss_locus_tag_idx ON antismash.cdss (locus_tag);
+
+
+CREATE TABLE IF NOT EXISTS antismash.gene_functions (
+    gene_function_id serial PRIMARY KEY,
+    cds_id int4 NOT NULL REFERENCES antismash.cdss ON DELETE CASCADE,
+    functional_class_id int4 NOT NULL REFERENCES antismash.functional_classes ON DELETE CASCADE
+);
+
+COMMENT ON TABLE antismash.gene_functions IS
+  'A table to link CDSs to functional classes.';
+
+CREATE INDEX IF NOT EXISTS gene_functions_cds_id_idx ON antismash.gene_functions (cds_id);
+CREATE INDEX IF NOT EXISTS gene_functions_functional_class_id_idx ON antismash.gene_functions (functional_class_id);
